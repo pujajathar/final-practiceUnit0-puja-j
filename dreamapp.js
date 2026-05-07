0c86c550b9e3ebfd6c36d3b7a4b9043dc87c2c2e
pujajathar <pujajathar91@.com>
2026-05-06 20:07:22 -0500

    dreamapp

--- dreamapp.js
+++ dreamapp.js
@@ -26,6 +26,7 @@ console.log(message); //logged message to the console
 /*Module- Creating and Using Objects*/
 //created object homeWork and assigned values to it.
 let homeWork =  { name: "Computer Science", completed: true, dueDate: "2026-05-05"};
+console.log(homeWork.name); //Accessed value of an object using "name" key. 
 
 /*Module- Creating or building Arrays--and--Module Creating and using Objects*/
 //created Array named homeWorkList which contains multiple objects. It also includes the object homeWork which
